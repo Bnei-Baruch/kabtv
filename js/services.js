56619fed1f2a2c89330ce31c14ddb3ad5b38953f
guya <guy.atia@gmail.com>
2014-04-04 04:12:00 +0300

    חיבור לדטה מהסרבר

--- js/services.js
+++ js/services.js
@@ -2,7 +2,7 @@ kabtv.service('getInitData', ['$http', 'pageSettings', function ($http, pageSett
     return $http.get('./json/onload.json');
 }]);
 kabtv.service('getHeadData', ['$http', 'pageSettings', function ($http, pageSettings) {
-    return $http.jsonp('http://api.kab.tv/api/nav_links', 
+    return $http.get('http://api.kab.tv/api/nav_links', 
     	{
     		responseType: 'json', 
     		params: {
@@ -21,9 +21,41 @@ kabtv.service('getTabsIframe', ['$http', function ($http) {
 }]);
 
 
-kabtv.service('setClipListes', ['$http', function ($http) {
-    return $http.get('./json/cliplistes.json');
+kabtv.service('setClipListes', ['$http', 'pageSettings', function ($http, pageSettings) {
+ 	return $http.get('http://api.kab.tv/api/categories', 
+    	{
+    		responseType: 'json', 
+    		params: {
+    			'lang': pageSettings.langVal
+    		},
+    		 headers: {
+		        'Content-Type': 'application/json'
+		    }
+    	}
+	);
+
 }]);
+
+
+
+kabtv.service('setClipList', ['$http', 'pageSettings', function ($http, pageSettings) {
+    function setClipList (id) {
+        return  (
+            $http.get('http://api.kab.tv/api/categories/'+id+'/vod_media', 
+                {
+                    responseType: 'json', 
+                     headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            )
+        );
+    }
+    return setClipList;
+
+}]);
+
+
 
 
 kabtv.service('getFooterData', ['$http', function ($http) {
