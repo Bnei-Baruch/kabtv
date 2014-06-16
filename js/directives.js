3757a2d518043b7c7ab2a943146c93dfdd5aad26
Edo Shor <edo.shor@pipl.com>
2014-06-16 04:15:22 +0300

    Fix creation of social share buttons on clip change

--- js/directives.js
+++ js/directives.js
@@ -63,22 +63,3 @@ kabtv.directive("kabtvClipList", function ( setClipList ) {
         }
     };
 });
-
-kabtv.directive('addthisToolbox', ['$timeout', function($timeout) {
-    return {
-        restrict : 'A',
-        transclude : true,
-        replace : true,
-        template : '<div ng-transclude></div>',
-        link : function($scope, element, attrs) {
-            $timeout(function () {
-                addthis.init();
-                addthis.toolbox(element);
-//                addthis.toolbox(element, {}, {
-//                    title : $scope.playerData.title,
-//                    description : $scope.playerData.description
-//                });
-            });
-        }
-    };
-}]);
