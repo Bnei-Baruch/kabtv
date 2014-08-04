28a904351eac12573dcc8abdc89e952f3fd8e5c4
Edo Shor <edo.shor@pipl.com>
2014-08-04 05:03:53 +0300

    prepare for first release to production

--- dist/js/2dc2adee.kabtv.templates.js
+++ dist/js/2dc2adee.kabtv.templates.js
@@ -17,12 +17,12 @@ angular.module('kabtv').run(['$templateCache', function($templateCache) {
 
 
   $templateCache.put('views/header.html',
-    "<header id=header><div class=\"tree toL\"><img src=./images/tree.gif></div><div class=whiteWrappwer><a href=\"./\" class=\"logoTop toL\"><img ng-src=./images/kab_tv_{{Lang.toLowerCase()}}.gif></a><div id=help-e class=toR><a href=\"http://live.kab.tv/client.php?locale=he&style=silver_he&group=3\" target=_blank onclick=\"if(navigator.userAgent.toLowerCase().indexOf('opera') != -1 && window.event.preventDefault) window.event.preventDefault();this.newWindow = window.open('http://live.kab.tv/client.php?locale=he&style=silver_he&group=3&url='+escape(document.location.href)+'&referrer='+escape(document.referrer), 'mibew', 'toolbar=0,scrollbars=0,location=0,status=1,menubar=0,width=640,height=480,resizable=1');this.newWindow.focus();this.newWindow.opener=window;return false\"><img ng-src={{helpImage}} alt=\"technical support\" border=0 width=226 height=50></a> </div><nav class=headLangNav><a ng-repeat=\"data in topMenuData\" ng-href={{data.url}} ng-class=currentLang(data.language)>{{data.caption}}</a></nav></div><nav class=headLinksNav><a ng-repeat=\"data in linksList\" ng-href={{data.url}} title={{data.title}}>{{data.caption}}</a></nav></header>"
+    "<header id=header ng-controller=kabtvHeader><div class=\"tree toL\"><img src=./images/tree.gif></div><div class=whiteWrappwer><a href=\"./\" class=\"logoTop toL\"><img ng-src=./images/kab_tv_{{Lang.toLowerCase()}}.gif></a><div id=help-e class=toR><a href=\"http://live.kab.tv/client.php?locale=he&style=silver_he&group=3\" target=_blank onclick=\"if(navigator.userAgent.toLowerCase().indexOf('opera') != -1 && window.event.preventDefault) window.event.preventDefault();this.newWindow = window.open('http://live.kab.tv/client.php?locale=he&style=silver_he&group=3&url='+escape(document.location.href)+'&referrer='+escape(document.referrer), 'mibew', 'toolbar=0,scrollbars=0,location=0,status=1,menubar=0,width=640,height=480,resizable=1');this.newWindow.focus();this.newWindow.opener=window;return false\"><img ng-src={{helpImage}} alt=\"technical support\" border=0 width=226 height=50></a> </div><nav class=headLangNav><a ng-repeat=\"data in topMenuData\" ng-href={{data.url}} ng-class=currentLang(data.language)>{{data.caption}}</a></nav></div><nav class=headLinksNav><a ng-repeat=\"data in linksList\" ng-href={{data.url}} title={{data.title}}>{{data.caption}}</a></nav></header>"
   );
 
 
   $templateCache.put('views/onload.html',
-    "<div class=\"{{dir}} allContent\"><div class=container><div kabtv-header></div><div kabtv-nav></div><div class=\"content clearfix\"><div kabtv-clip-list class=\"aside toR clipList clearfix\">111</div><div kabtv-tabs class=\"aside toL clearfix\">222</div><div class=mid ng-view></div></div><div kabtv-footer class=footer></div></div></div>"
+    ""
   );
 
 
@@ -37,12 +37,12 @@ angular.module('kabtv').run(['$templateCache', function($templateCache) {
 
 
   $templateCache.put('views/tabs.html',
-    "<div class=tabs id=asideTabIframe><div ng-repeat=\"item in tabs\" class=\"tabBtn {{currentTab == item ? 'select': '';}}\" ng-click=switchTab(item)>{{item | translate}}</div><div ng-switch=currentTab class=forIframe><div kabtv-updates ng-switch-when=TAB_UPDATES class=updates></div><iframe ng-switch-when=TAB_SCHEDULE ng-src={{currentTabUrl}} frameborder=0></iframe><iframe ng-switch-when=TAB_QUESTIONS ng-src={{currentTabUrl}} frameborder=0 scrolling=no></iframe><iframe ng-switch-when=TAB_SKETCHES ng-src={{currentTabUrl}} frameborder=0></iframe></div></div>"
+    "<div class=tabs id=asideTabIframe ng-controller=kabtvTabs><div ng-repeat=\"item in tabs\" class=\"tabBtn {{currentTab == item ? 'select': '';}}\" ng-click=switchTab(item)>{{item | translate}}</div><div ng-include=\"currentTab + '.tpl'\" class=forIframe></div></div><script type=text/ng-template id=TAB_UPDATES.tpl><div class=\"updates\" ng-include=\"'views/updates.html'\"></div></script><script type=text/ng-template id=TAB_SCHEDULE.tpl><iframe ng-src=\"{{currentTabUrl}}\" frameborder=\"0\" scroll></iframe></script><script type=text/ng-template id=TAB_QUESTIONS.tpl><iframe ng-src=\"{{currentTabUrl}}\" frameborder=\"0\"></iframe></script><script type=text/ng-template id=TAB_SKETCHES.tpl><iframe ng-src=\"{{currentTabUrl}}\" frameborder=\"0\"></iframe></script>"
   );
 
 
   $templateCache.put('views/updates.html',
-    "<div ng-repeat=\"update in updates\" class=\"item claerfix\"><h5 ng-bind-html=update.title></h5><div class=description ng-bind-html=update.description></div><a ng-href={{update.url}} class=\"url textToR\" ng-bind-html=update.url_caption></a></div>"
+    "<div ng-repeat=\"update in updates\" class=\"item clearfix\"><div class=ad ng-if=update.url><div class=header><a ng-href={{update.url}} target=_blank><span class=title ng-bind-html=update.title></span> <span class=subtitle ng-bind-html=update.subtitle></span></a></div><div class=body ng-if=update.description><div class=description ng-bind-html=update.description></div></div><div class=body ng-if=update.image_url><a ng-href={{update.url}} target=_blank><img ng-src={{update.image_url}}></a></div><a ng-if=update.url_caption ng-href={{update.url}} class=\"url toR\" target=_blank>{{update.url_caption}}>></a></div><div class=ad ng-if=!update.url><div class=header><span class=title ng-bind-html=update.title></span> <span class=subtitle ng-bind-html=update.subtitle></span></div><div class=body ng-if=update.description><div class=description ng-bind-html=update.description></div></div><div class=body ng-if=update.image_url><img ng-src={{update.image_url}}></div></div></div>"
   );
 
 }]);
