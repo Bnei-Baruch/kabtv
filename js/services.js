1cf160c79fd7da6f615765cbbb3bd33d2ba1f6a4
Edo Shor <edo.shor@pipl.com>
2014-11-04 03:41:10 +0200

    Fix for dynamic streams service to use the url from the DB instead of building it in the client

--- js/services.js
+++ js/services.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 angular.module('kabtv')
     /*data that using on start*/
     .service('initData', function () {
@@ -137,9 +136,6 @@ angular.module('kabtv')
         function ($http, pageSettings, API_BASE) {
             var _svc = {}, _param = {};
 
-            //var API_BASE = 'http://localhost:3000/api/';
-            //var API_BASE = 'http://cryptic-thicket-2657.herokuapp.com/api/';
-//            var API_BASE = 'http://kab.tv/api/';
             var defParam = {
                 params: {
                     'lang': pageSettings.Lang
@@ -154,6 +150,14 @@ angular.module('kabtv')
                 return $http.get(API_BASE + 'nav_links', _param);
             };
 
+            _svc.getFooterData = function () {
+                _param.params = {
+                    'placeholder': 'footer'
+                };
+                angular.extend(_param.params, defParam.params);
+                return $http.get(API_BASE + 'nav_links', _param);
+            };
+
             _svc.setClipListes = function () {
                 return $http.get(API_BASE + 'categories', defParam);
             };
@@ -161,38 +165,30 @@ angular.module('kabtv')
             _svc.setClipList = function (id) {
                 return $http.get(API_BASE + 'categories/' + id + '/vod_media');
             };
+
             _svc.getClipById = function (id) {
                 return $http.get(API_BASE + 'vod_media/' + id);
             };
-            _svc.getFooterData = function (id) {
-                _param.params = {
-                    'placeholder': 'footer'
-                };
-                angular.extend(_param.params, defParam.params);
-                return $http.get(API_BASE + 'nav_links', _param);
-            };
-            _svc.getOnlineMedia = function (id) {
+
+
+            _svc.getOnlineMedia = function () {
                 return $http.get(API_BASE + 'streams');
             };
-            _svc.getUpdates = function (id) {
+
+            _svc.getUpdates = function () {
                 return $http.get(API_BASE + 'updates', defParam);
             };
+
             _svc.getEventStatus = function() {
                 return $http.get(API_BASE + 'event_status', defParam);
             };
+
             _svc.getDynamicGeoStream = function(pObj) {
-                var url = "http://streams.kab.tv/kabtv-" + pObj.language.toLowerCase() + "-" + pObj.quality + ".js";
-                var conf = {
-                    // callback: DynamicGeoStreamLocator 
-                };
-                return $http.jsonp(url, conf).success(function(e){
+                return $http.jsonp(pObj.url, {}).success(function(e){
                     return e;
                 });
-                /* return $http.jsonp(url, conf).then(function(e){
-                 return e;
-                 });*/
             };
+
             return _svc;
 
         }]);
-
