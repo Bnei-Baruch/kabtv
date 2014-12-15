c0e5861baf013c38bf2587a17bca49a6efc58e5f
Edo Shor <edo.shor@pipl.com>
2014-12-15 04:38:56 +0200

    Rewrite - First commit of new dist build

--- app/core/constants.js
+++ app/core/constants.js
@@ -1,8 +1,11 @@
-(function() {
-    'use strict';
+(function(angular, undefined) {
+'use strict';
 
-    angular.module('app.core')
-        .constant('API_BASE', 'http://dev.kab.tv/api/')
-        .constant('HELP_IMAGE_BASE', 'http://live.kab.tv/button.php?image=tech');
+angular.module('app.core')
 
-})();
+.constant('API_BASE', 'http://dev.kab.tv/api/')
+
+.constant('HELP_IMAGE_BASE', 'http://live.kab.tv/button.php?image=tech')
+
+;
+})(angular);
